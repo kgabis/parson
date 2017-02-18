2bfa4153db57b6e47076d9c3f4744920af435c04
Krzysztof Gabis <kgabis@gmail.com>
2017-02-18 16:41:14 +0000

    Updates copyright.

--- parson.c
+++ parson.c
@@ -1,6 +1,6 @@
 /*
  Parson ( http://kgabis.github.com/parson/ )
- Copyright (c) 2012 - 2016 Krzysztof Gabis
+ Copyright (c) 2012 - 2017 Krzysztof Gabis
 
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
