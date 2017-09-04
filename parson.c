d485b068c738f268b12f889249532a84244ca27d
Krzysztof Gabis <kgabis@gmail.com>
2017-09-04 19:56:17 +0100

    Fixes a memory leak (issue #82).

--- parson.c
+++ parson.c
@@ -669,8 +669,13 @@ static JSON_Value * parse_object_value(const char **string, size_t nesting) {
     }
     while (**string != '\0') {
         new_key = get_quoted_string(string);
+        if (new_key == NULL) {
+            json_value_free(output_value);
+            return NULL;
+        }
         SKIP_WHITESPACES(string);
-        if (new_key == NULL || **string != ':') {
+        if (**string != ':') {
+            parson_free(new_key);
             json_value_free(output_value);
             return NULL;
         }
