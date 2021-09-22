3cde2bb3ae173642b31ac790101584658915ddb3
davidalo <davidalto97@gmail.com>
2021-09-22 16:56:16 +0200

    Refactor FLOAT_FORMAT to make it configurable at compile time (#2)
    Co-authored-by: David Alonso de la Torre <david.alonso@sidisel.com>

--- parson.c
+++ parson.c
@@ -63,7 +63,9 @@
 #define STARTING_CAPACITY 16
 #define MAX_NESTING       2048
 
-#define FLOAT_FORMAT "%1.17g" /* do not increase precision without incresing NUM_BUF_SIZE */
+#ifndef PARSON_FLOAT_FORMAT
+#define PARSON_FLOAT_FORMAT "%1.17g" /* do not increase precision without incresing NUM_BUF_SIZE */
+#endif
 #define NUM_BUF_SIZE 64 /* double printed with "%1.17g" shouldn't be longer than 25 bytes so let's be paranoid and use 64 */
 
 #define SIZEOF_TOKEN(a)       (sizeof(a) - 1)
